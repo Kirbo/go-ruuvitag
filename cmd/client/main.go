ed32cccd275eeff0af24e40e88ad53ce7f7fb92a
Kimmo Saari <kirbownz@me.com>
2020-10-08 17:09:52 +0300

    Client socket.io

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,25 +8,35 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
+	socketio "github.com/googollee/go-socket.io"
 	"github.com/imdario/mergo"
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/kirbo/go-ruuvitag/internal/channels"
-	"github.com/kirbo/go-ruuvitag/internal/models"
 	"github.com/patrickmn/go-cache"
 	"github.com/peknur/ruuvitag"
+
+	"github.com/kirbo/go-ruuvitag/internal/channels"
+	"github.com/kirbo/go-ruuvitag/internal/models"
 )
 
 // Cache for variables
 var (
 	rdb    *redis.Client
 	config models.JsonDevices
+	server *socketio.Server
 
-	Cache    *cache.Cache    = cache.New(0, 0)
-	ctx      context.Context = context.Background()
-	interval time.Time       = time.Minute
+	Cache        *cache.Cache    = cache.New(0, 0)
+	ctx          context.Context = context.Background()
+	interval     time.Duration   = time.Minute
+	namespace    string          = "/"
+	room         string          = ""
+	updateEvent  string          = "update"
+	initialEvent string          = "initial"
 )
 
 func connectRedis() {
@@ -87,6 +97,87 @@ func startTickers() {
 	}()
 }
 
+func GinMiddleware(allowOrigin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Request.Header.Del("Origin")
+
+		c.Next()
+	}
+}
+
+func startSocketIOServer() {
+	var err error
+
+	router := gin.New()
+
+	server, err = socketio.NewServer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	server.OnConnect(namespace, func(s socketio.Conn) error {
+		id := s.ID()
+		s.Join(room)
+		clientCount := server.Count()
+		fmt.Printf("clientCount: %v    connected ID '%v'\n", clientCount, id)
+
+		devices, err := initialDataForWebClient()
+		if err != nil {
+			panic(err)
+		}
+
+		server.BroadcastToRoom(namespace, room, initialEvent, devices)
+
+		return nil
+	})
+
+	server.OnError(namespace, func(s socketio.Conn, e error) {
+		s.Close()
+		clientCount := server.Count()
+		fmt.Printf("clientCount: %v    error %+v\n", clientCount, e)
+	})
+
+	server.OnDisconnect(namespace, func(s socketio.Conn, reason string) {
+		id := s.ID()
+		s.Close()
+		clientCount := server.Count()
+		fmt.Printf("clientCount: %v disconnected ID '%v'\n", clientCount, id)
+	})
+
+	wg := sync.WaitGroup{}
+
+	go func() {
+		err = server.Serve()
+		if err != nil {
+			panic(err)
+		}
+		wg.Done()
+	}()
+	wg.Add(1)
+
+	defer server.Close()
+
+	router.Use(GinMiddleware("*"))
+	router.Use(static.Serve("/", static.LocalFile("./floorplan/dist", true)))
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+
+	err = router.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func createInserts() {
 	for i := range config {
 		sensor := &config[i]
@@ -127,6 +218,64 @@ func makeTimestamp() int64 {
 
 func parseOldID(id string) string {
 	return strings.ToLower(strings.Replace(id, ":", "", -1))
+}
+
+func parseMessage(row string) (device models.Device, err error) {
+	err = json.Unmarshal([]byte(row), &device)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func broadcastMessage(device models.Device) models.BroadcastMessage {
+	return models.BroadcastMessage{
+		Timestamp:   device.TimestampZ,
+		TagID:       device.OldID,
+		Name:        device.Name,
+		Temperature: device.Temperature,
+		Pressure:    device.Pressure,
+		Ping:        device.Ping,
+		Battery:     device.Battery,
+		Humidity:    device.Humidity,
+	}
+}
+
+func broadcastDevice(row string) {
+	device, err := parseMessage(row)
+	if err != nil {
+		panic(err)
+	}
+
+	broadcastMsg := broadcastMessage(device)
+
+	server.BroadcastToRoom(namespace, room, updateEvent, broadcastMsg)
+}
+
+func initialDataForWebClient() (devices []models.BroadcastMessage, err error) {
+	iter := rdb.Scan(ctx, 0, fmt.Sprintf("%s%s", channels.Device, "*"), 1).Iterator()
+	for iter.Next(ctx) {
+		key := iter.Val()
+
+		row, err := rdb.Get(ctx, key).Result()
+		if err != nil {
+			log.Printf("No data found for: %s", key)
+			panic(err)
+		}
+
+		device, err := parseMessage(row)
+		if err != nil {
+			panic(err)
+		}
+
+		devices = append(devices, broadcastMessage(device))
+	}
+	if err := iter.Err(); err != nil {
+		panic(err)
+	}
+
+	return
 }
 
 func handler(data ruuvitag.Measurement) {
@@ -176,12 +325,14 @@ func handler(data ruuvitag.Measurement) {
 	Cache.Set(fmt.Sprintf("%s%s", channels.Device, address), device, cache.NoExpiration)
 	Cache.Set(fmt.Sprintf("lastTimestamp:%s", address), timestamp, cache.NoExpiration)
 
-	log.Printf("%s[v%d] %s : %+v", address, data.Format(), name, deviceStub)
+	// log.Printf("%s[v%d] %s : %+v", address, data.Format(), name, deviceStub)
 
 	redisData, err := json.Marshal(device)
 	if err != nil {
 		panic(err)
 	}
+
+	broadcastDevice(string(redisData))
 
 	if err = setAndPublish(fmt.Sprintf("%s%s", channels.Device, addressOld), string(redisData)); err != nil {
 		panic(err)
@@ -190,6 +341,7 @@ func handler(data ruuvitag.Measurement) {
 
 func main() {
 	connectRedis()
+	go startSocketIOServer()
 	loadConfigs()
 	startTickers()
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kirbo/go-ruuvitag
 go 1.15
 
 require (
+	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.2.3
