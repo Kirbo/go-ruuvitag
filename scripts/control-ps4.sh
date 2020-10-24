ccee470aa6cf807890e566909ed012e631315820
Kimmo Saari <kirbownz@me.com>
2020-10-25 00:04:42 +0300

    fix: fixed typo

--- scripts/control-ps4.sh
+++ scripts/control-ps4.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
 
-printUsage() {
-  echo -ne "${GREEN}"
- cat << EOF
-###############################################################
-#
-#  Usage:
-#      ./scripts/control-ps4.sh [args]
-#
-#  Parameters:
-#
-#     Name            Required    Default value     Description
-#
-#     args            No                            Acceptable value: standby
-#
-#    Examples:
-#       # Turn on the PS4
-#           ./scripts/control-ps4.sh
-#
-#       # Turn off the PS4
-#           ./scripts/control-ps4.sh standby
-#
-###############################################################
-EOF
-echo -ne "${NORMAL}"
-}
-
-
-SCRIPTS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-source "${SCRIPTS_DIR}/_includes/_main.sh"
-
-ARGS=$@
-
-/home/pi/.yarn/bin/ps4-waker -c /home/pi/.ps4-wake.credentials.json -d 192.168.1.207 --pass 1337 ${ARGS} >> ~/logs/control-ps4.log
+/home/pi/.yarn/bin/ps4-waker -c /home/pi/.ps4-wake.credentials.json -d 192.168.1.207 --pass 1337 $@ >> ~/logs/control-ps4.log
