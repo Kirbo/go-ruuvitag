c456afc9deb7e0e2a1c1bcd662c606443de6fdaa
Kimmo Saari <kirbownz@me.com>
2020-10-24 23:34:29 +0300

    fix: fixed typo

--- scripts/control-ps4.sh
+++ scripts/control-ps4.sh
@@ -40,4 +40,4 @@ if [[ -z "${ARGS}" ]]; then
     exit
 fi
 
-/home/pi/.yarn/bin/ps4-waker -c /home/pi/.ps4-wake.credentials.json -d 192.168.1.207 --pass 1337 standby ${ARGS}
+/home/pi/.yarn/bin/ps4-waker -c /home/pi/.ps4-wake.credentials.json -d 192.168.1.207 --pass 1337 standby ${ARGS} > ~/logs/control-ps4.log
